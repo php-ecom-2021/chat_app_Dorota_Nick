306e5298794251df732f9d197518043ba845398b
NikolayR21 <1081601@ucn.dk>
2021-12-11 01:54:10 +0100

    updated messages styles

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chat;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MessagesController extends Controller
@@ -13,4 +15,17 @@ class MessagesController extends Controller
     public function index(){
         return view('messages.messages');
     }
+
+    public function show(Request $request){
+
+    }
 }
+
+
+/*
+get all chats with the currently logged in user
+
+select a user
+if a chat doesnt exist, create one
+else take the one that exists
+*/
--- resources/views/messages/messages.blade.php
+++ resources/views/messages/messages.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="w-full h-screen flex">
-    <div class="w-2/12 h-100 bg-blue-100">
+    <div class="w-2/12 h-100">
         <div class="flex justify-between p-4 w-full">
             @if(auth()->user()->avatar)
-                <img class="w-16 h-16 rounded-full mr-4" src="{{asset('images/'.auth()->user()->avatar)}}">
+                <img class=" w-16 h-16 rounded-full mr-4" src="{{asset('images/'.auth()->user()->avatar)}}">
             @else
-                <img class="w-16 h-16 rounded-full mr-4" src="{{asset('images/default_avatar.jpg')}}">
+                <img class=" w-16 h-16 rounded-full mr-4" src="{{asset('images/default_avatar.jpg')}}">
             @endif
             <div class="w-full">
                 <h2 class="text-xl font-bold pb-2">{{auth()->user()->name}}</h2>
@@ -28,7 +28,7 @@
                 </button>
             </form>
         </div>
-        <div class="w-full p-4">
+        <div class="w-full p-4 shadow-md">
             <form action="{{route('search')}}" method="get" name="search">
                 @csrf
                 <label for="search" class="sr-only">Search users</label>
@@ -39,8 +39,41 @@
             
         </div>
     </div>
-    <div class="w-10/12">
-        @yield('chat')
+    <div class="w-10/12 h-screen">
+        <div class="chat w-full h-screen relative">
+            <header class="chatHeader w-full top-0 flex p-4 shadow-md">
+                <img class=" w-16 h-16 rounded-full mr-4" src="{{asset('images/default_avatar.jpg')}}">
+                <div class="w-full">
+                    <h2 class="text-xl font-bold">Username</h2>
+                </div>
+                <div class="flex self-center">
+                    <form action="">
+                        @csrf
+                        <button type="submit" class="px-4 py-2 mr-4 rounded-full font-bold bg-gray-200 text-red-500">Delete&nbsp;chat</button>
+                    </form>
+                    <form action="">
+                        @csrf
+                        <button type="submit" class="px-4 py-2 rounded-full font-bold bg-gray-200">Unfriend</button>
+                    </form>
+                </div>
+            </header>
+            <div class="chatHistory w-full p-4">
+
+            </div>
+            <form class="p-4 flex absolute bottom-0 w-full" action="" method="post">
+                <label for="chatInput" class="sr-only">Chat Input</label>
+                <input class="px-4 py-2 rounded-full mr-4 bg-gray-200 w-full" type="text" name="chatInput" placeholder="Enter a message...">
+                <button class="px-4 py-2 rounded-full text-blue-500 font-bold bg-gray-200" type="submit">Send</button>
+            </form>
+        </div>
+        <div class="empty w-full h-screen grid place-items-center">
+            <div class="w-3/12 text-center text-gray-300">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-48 w-48 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                </svg>
+                <p class="font-bold text-4xl">Select a user and start chatting.</p>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
